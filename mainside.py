6b2f90549678c02df6dd07016e1132e0bea5ae85
loverikim <165342135+loverikim@users.noreply.github.com>
2024-07-04 10:28:39 +0900

    Update mainside.py

--- mainside.py
+++ mainside.py
@@ -9,15 +9,15 @@ user_password = st.sidebar.text_input('패스워드(Password) 입력', value="",
 if user_password == '1234':
 
     st.sidebar.header("포트폴리오")
-    selectbox_options = ["",'탐색적데이터분석','머신러닝_예측'] # 셀렉트 박스의 선택 항목
+    selectbox_options = ["",'탐색적 데이터 분석','머신러닝_예측'] # 셀렉트 박스의 선택 항목
     your_option = st.sidebar.selectbox('메뉴?', selectbox_options, index=0) # 셀렉트박스의 항목 선택 결과
     st.sidebar.write('**당신의 선택**:', your_option)
 
     # 메인(Main) 화면
     st.subheader(your_option, divider='rainbow')
     
-    if your_option == "탐색적데이터분석":
-        st.write("탐색적데이터분석")   
+    if your_option == "탐색적 데이터 분석":
+        st.write("탐색적 데이터 분석")   
         np.np_main()
     elif your_option == "머신러닝_예측":
         st.write("머신러닝_예측")
